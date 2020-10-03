dda14843d6bfa1acdf31c11b8156553391b7fd89
Chang <cnxiong03@gmail.com>
2020-10-02 23:20:53 -0400

    else if practice

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,21 @@
+package com.codewithchang;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if(number % 5 == 0 && number % 3 == 0)
+        System.out.print("FizzBuzz");
+        else if(number % 5 == 0)
+            System.out.print("Fizz");
+        else if(number % 3 == 0)
+            System.out.print("Buzz");
+        else
+            System.out.println(number);
+    }
+}
